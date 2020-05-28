49a858a18ae35507ba2c505011330bf90c2e905d
ethanrdj <ethanrdavis96@gmail.com>
2020-05-28 13:45:34 +0100

    added setSail function and test for it

--- __test__/Ship.test.js
+++ __test__/Ship.test.js
@@ -5,10 +5,19 @@ describe('Ship', () => {
     it('can be instantiated', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     });
-    
+
     it('has a starting port', () => {
         const ship = new Ship('Dover');
 
         expect(ship.startingPort).toBe('Dover');
+    });
+});
+
+describe('setSail', () => {
+    it('changes the starting port value to falsy', () => {
+        const ship = new Ship('Dover');
+        ship.setSail();
+
+        expect(ship.startingPort).toBeFalsy();
     });
 });
