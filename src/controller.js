a31faaa12589625465dac8d7d8ca0e791afd4e83
ethanrdj <ethanrdavis96@gmail.com>
2020-06-07 12:00:41 +0100

    ship can dock at each port

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,12 @@
 (function exportController() {
 class Controller {
-    constructor () {
+    constructor (ship) {
+        this.ship = ship;
         this.initialiseSea()
+
+        document.querySelector('#sailbutton').addEventListener('click', () => {
+          this.setSail();
+        });
     };
 
     initialiseSea() {
@@ -34,14 +39,41 @@ class Controller {
         });
     };
 
-    renderShip(ship) {
+    renderShip() {
+      const ship = this.ship
+    
       const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
-      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`)
-
+      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+    
       const shipElement = document.querySelector('#ship');
       shipElement.style.top = `${portElement.offsetTop + 32}px`;
-      shipElement.style.left = `${portElement.offsetLef - 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft - 32}px`;
     };
+
+    setSail() {
+      const ship = this.ship;
+
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index = '${nextPortIndex}']`);
+
+      if (!nextPortElement) {
+        return alert('End of the line!');
+      }
+
+      const shipElement = document.querySelector('#ship');
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.style.left, 10);
+        if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+          ship.dock();
+          clearInterval(sailInterval);
+        }
+      
+        shipElement.style.left = `${shipLeft + 1}px`;
+      }, 20);
+      
+      ship.setSail();
+  };
 };
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
